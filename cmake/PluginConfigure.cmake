2dad9be5770c98f0a8cc456724bb4c02419d6ad8
Pavel Kalian <pavel@kalian.cz>
2016-08-09 11:54:25 -0300

    Update cmake infrastructure

--- cmake/PluginConfigure.cmake
+++ cmake/PluginConfigure.cmake
@@ -15,7 +15,14 @@ ENDIF (COMMAND cmake_policy)
 
 MESSAGE (STATUS "*** Staging to build ${PACKAGE_NAME} ***")
 
-configure_file(cmake/version.h.in ${PROJECT_SOURCE_DIR}/src/version.h)
+#configure_file(cmake/version.h.in ${PROJECT_SOURCE_DIR}/src/version.h)
+#  Do the version.h configuration into the build output directory,
+#  thereby allowing building from a read-only source tree.
+IF(NOT SKIP_VERSION_CONFIG)
+    configure_file(cmake/version.h.in ${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/include/version.h)
+    INCLUDE_DIRECTORIES(${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/include)
+ENDIF(NOT SKIP_VERSION_CONFIG)
+
 SET(PACKAGE_VERSION "${VERSION_MAJOR}.${VERSION_MINOR}" )
 
 #SET(CMAKE_BUILD_TYPE Debug)
@@ -48,18 +55,30 @@ IF(MSVC)
     ADD_DEFINITIONS(-D_CRT_NONSTDC_NO_DEPRECATE -D_CRT_SECURE_NO_DEPRECATE)
 ENDIF(MSVC)
 
-SET(wxWidgets_USE_LIBS base core net xml html adv)
-SET(BUILD_SHARED_LIBS TRUE)
 
-FIND_PACKAGE(wxWidgets REQUIRED)
+SET_PROPERTY(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS TRUE)
+SET(BUILD_SHARED_LIBS "ON")
+
+#  QT_ANDROID is a cross-build, so the native FIND_PACKAGE(wxWidgets...) and wxWidgets_USE_FILE is not useful.
+IF(NOT QT_ANDROID)
+IF(NOT DEFINED wxWidgets_USE_FILE)
+  SET(wxWidgets_USE_LIBS base core net xml html adv)
+  SET(BUILD_SHARED_LIBS TRUE)
+  FIND_PACKAGE(wxWidgets REQUIRED)
+ENDIF(NOT DEFINED wxWidgets_USE_FILE)
+
+  INCLUDE(${wxWidgets_USE_FILE})
+ENDIF(NOT QT_ANDROID)
+
 
 IF(MSYS)
 # this is just a hack. I think the bug is in FindwxWidgets.cmake
 STRING( REGEX REPLACE "/usr/local" "\\\\;C:/MinGW/msys/1.0/usr/local" wxWidgets_INCLUDE_DIRS ${wxWidgets_INCLUDE_DIRS} )
 ENDIF(MSYS)
 
-INCLUDE(${wxWidgets_USE_FILE})
-
+#  QT_ANDROID is a cross-build, so the native FIND_PACKAGE(OpenGL) is not useful.
+#
+IF (NOT QT_ANDROID )
 FIND_PACKAGE(OpenGL)
 IF(OPENGL_GLU_FOUND)
 
@@ -73,6 +92,73 @@ IF(OPENGL_GLU_FOUND)
 ELSE(OPENGL_GLU_FOUND)
     MESSAGE (STATUS "OpenGL not found..." )
 ENDIF(OPENGL_GLU_FOUND)
+ENDIF(NOT QT_ANDROID)
+
+#  Building for QT_ANDROID involves a cross-building environment,
+#  So the OpenGL include directories, flags, etc must be stated explicitly
+#  without trying to locate them on the host build system.
+IF(QT_ANDROID)
+    MESSAGE (STATUS "Using GLESv1 for Android")
+    ADD_DEFINITIONS(-DocpnUSE_GLES)
+    ADD_DEFINITIONS(-DocpnUSE_GL)
+    ADD_DEFINITIONS(-DUSE_GLU_TESS)
+    ADD_DEFINITIONS(-DARMHF)
+   
+    SET(OPENGLES_FOUND "YES")
+    SET(OPENGL_FOUND "YES")
+
+#    SET(wxWidgets_USE_LIBS ${wxWidgets_USE_LIBS} gl )
+#    add_subdirectory(src/glshim)
+
+#    add_subdirectory(src/glu)
+
+ELSE(QT_ANDROID)
+    FIND_PACKAGE(OpenGL)
+    IF(OPENGL_GLU_FOUND)
+
+        SET(wxWidgets_USE_LIBS ${wxWidgets_USE_LIBS} gl)
+        INCLUDE_DIRECTORIES(${OPENGL_INCLUDE_DIR})
+
+        MESSAGE (STATUS "Found OpenGL..." )
+        MESSAGE (STATUS "    Lib: " ${OPENGL_LIBRARIES})
+        MESSAGE (STATUS "    Include: " ${OPENGL_INCLUDE_DIR})
+        ADD_DEFINITIONS(-DocpnUSE_GL)
+    ELSE(OPENGL_GLU_FOUND)
+        MESSAGE (STATUS "OpenGL not found..." )
+    ENDIF(OPENGL_GLU_FOUND)
+
+ENDIF(QT_ANDROID)
+
+# On Android, PlugIns need a specific linkage set....
+IF (QT_ANDROID )
+  # These libraries are needed to create PlugIns on Android.
+
+  SET(OCPN_Core_LIBRARIES
+        # Presently, Android Plugins are built in the core tree, so the variables {wxQT_BASE}, etc.
+        # flow to this module from above.  If we want to build Android plugins out-of-core, this will need improvement.
+
+        # TODO This is pretty ugly, but there seems no way to avoid specifying a full path in a cross build....
+        /home/dsr/Projects/opencpn_sf/opencpn/build-opencpn-Android_for_armeabi_v7a_GCC_4_8_Qt_5_5_0-Debug/libopencpn.so
+
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_baseu-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_qtu_core-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_qtu_html-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_baseu_xml-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_qtu_qa-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_qtu_adv-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_qtu_aui-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_baseu_net-3.1-arm-linux-androideabi.a
+        ${wxQt_Base}/${wxQt_Build}/lib/libwx_qtu_gl-3.1-arm-linux-androideabi.a
+        ${Qt_Base}/android_armv7/lib/libQt5Core.so
+        ${Qt_Base}/android_armv7/lib/libQt5OpenGL.so
+        ${Qt_Base}/android_armv7/lib/libQt5Widgets.so
+        ${Qt_Base}/android_armv7/lib/libQt5Gui.so
+        ${Qt_Base}/android_armv7/lib/libQt5AndroidExtras.so
+
+        ${NDK_Base}/sources/cxx-stl/gnu-libstdc++/4.8/libs/armeabi-v7a/libgnustl_shared.so
+        )
+
+ENDIF(QT_ANDROID)
 
 SET(BUILD_SHARED_LIBS TRUE)
 
